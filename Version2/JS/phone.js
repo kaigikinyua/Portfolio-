724ed3477ed9ca8bcc16de5e982a18d63bb0095a
kaigikinyua <kaigikinyua@gmail.com>
2019-02-11 11:51:58 +0300

    Added the phone version

---
+++ Version2/JS/phone.js
@@ -0,0 +1,8 @@
+function hideNavbar(){
+  document.getElementById('navbar').style.visibility="hidden";
+  document.getElementById('addNav').style.visibility="visible";
+}
+function restoreNav(){
+  document.getElementById('navbar').style.visibility="visible";
+  document.getElementById('addNav').style.visibility="hidden";
+}
