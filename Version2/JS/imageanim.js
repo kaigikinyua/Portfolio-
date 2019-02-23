2b2d07eb34e457335b6eb5121fed1cd1f32795b1
kaigikinyua <kaigikinyua@gmail.com>
2019-02-23 23:58:20 +0300

    working solution

--- Version2/JS/imageanim.js
+++ Version2/JS/imageanim.js
@@ -2,24 +2,30 @@ console.log("Image animation online");
 //functions helping in the manipulation of images
 var counter=0;
 var previous=0;
-var images=document.getElementsByClassName('ImageSlide');
+let images=document.querySelectorAll('.ImageSlide');
 console.log(images[2]);
 function reset(){
-  for(var i=0;i<=images.length;i++){
-    images[i].style.display="hidden";
+  for(var i=0;i<images.length;i++){
+    images[i].style.display="none";
   }
 }
 function nextImage(){
   reset();
-  for(counter=0;counter<=images.length;counter++){
-  images[counter].style.visibility="visible";
-  console.log(counter);
-  }
+  images[counter].style.display="block";
+  counter++;
   if(counter>=images.length){
     counter=0;
   }
 }
-reset();
+function previousImage(){
+  reset();
+  images[counter].style.display="block";
+  counter--;
+  if(counter<=0){
+    counter=images.length;
+  }
+}
+//nextImage();
 //function previousImage(){}
 //setInterval(nextImage,1000);
 //nextImage();
